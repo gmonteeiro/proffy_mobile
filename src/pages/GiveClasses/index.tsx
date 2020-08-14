cb9fc3448f887613cc792eb4c8bcba0e63929e43
Gabriel <gmonteeiro@gmail.com>
2020-08-13 21:10:22 -0300

    initializing the give_class page

---
+++ src/pages/GiveClasses/index.tsx
@@ -0,0 +1,28 @@
+import React from 'react';
+import { View, ImageBackground, Text } from 'react-native';
+
+import giveClassesBgImage from '../../assets/images/give-classes-background.png';
+
+import styles from './styles';
+
+function GiveClasses(){
+    return (
+        <View  style={styles.container}>
+            <ImageBackground 
+                resizeMode="contain" 
+                source={giveClassesBgImage} 
+                style={styles.content}
+            >
+
+                <Text style={styles.title}>Quer ser um Proffy</Text>
+                <Text style={styles.description}> 
+                    Para começar, você precisa se cadastrar como professor na nossa plataforma web.
+                </Text>
+
+
+            </ImageBackground>
+        </View>
+    )
+}
+
+export default GiveClasses;
---
+++ src/pages/GiveClasses/styles.ts
@@ -0,0 +1,38 @@
+import { StyleSheet } from 'react-native';
+
+// importar font do expor google fonts
+// expo install expo-font @expo-google-fonts/nome_da_fonte
+
+const styles = StyleSheet.create({
+    container:{
+        flex: 1,
+        backgroundColor: '#8257e5',
+        justifyContent: 'center',
+        padding:40
+    },
+
+    content:{
+        flex:1,
+        justifyContent:'center'
+    },
+
+    title:{
+        fontFamily: 'Archivo_700Bold',
+        color:'#ffffff',
+        fontSize: 32,
+        lineHeight: 37,
+        maxWidth: 180
+    },
+
+    description:{
+        marginTop: 24,
+        color: '#d4c2ff',
+        fontSize: 16,
+        lineHeight: 26,
+        fontFamily: 'Poppins_400Regular',
+        maxWidth: 240
+    }
+
+});
+
+export default styles;
